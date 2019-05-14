e66cfbc8f25608f8366f09842eb298826671b8ab
Paweł <example@mail.com>
2019-05-14 12:49:26 +0200

    Dodano nowe zapytanie klasy

---
+++ Controllers/KlasyController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PlanLekcji.DTO;
+
+namespace PlanLekcji.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KlasyController : ControllerBase
+    {
+        IConfiguration configuration;
+        public KlasyController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IList<KlasyDTO>> GetAll()
+        {
+            List<KlasyDTO> list = new List<KlasyDTO>();
+             using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+
+                SqlCommand command = new SqlCommand(@"SELECT K.Id, K.Nazwa, K.Rocznik, N.Imie, N.Nazwisko FROM Klasy K join Nauczyciele N on K.WychowawcaId = N.Id", connection);
+                command.CommandType = System.Data.CommandType.Text;
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    KlasyDTO dto = new KlasyDTO();
+                    dto.Id = reader.GetInt64(0);
+                    dto.Nazwa = reader.GetString(1);
+                    dto.Rocznik = reader.GetInt32(2);
+                    dto.Imie = reader.GetString(3);
+                    dto.Nazwisko = reader.GetString(4);
+                    list.Add(dto);
+                }
+                connection.Close();
+            }
+            return list;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<KlasyDTO> Get(Int64 id)
+        {
+            KlasyDTO dto = new KlasyDTO();
+            using (SqlConnection connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+
+                SqlCommand command = new SqlCommand(@"SELECT K.Id, K.Nazwa, K.Rocznik, N.Imie, N.Nazwisko FROM Klasy K join Nauczyciele N on K.WychowawcaId = N.Id WHERE K.Id = @Id", connection);
+                command.CommandType = System.Data.CommandType.Text;
+                command.Parameters.Add("Id", SqlDbType.BigInt);
+                command.Parameters["Id"].Value = id;
+                connection.Open();
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    dto.Id = reader.GetInt64(0);
+                    dto.Nazwa = reader.GetString(1);
+                    dto.Rocznik = reader.GetInt32(2);
+                    dto.Imie = reader.GetString(3);
+                    dto.Nazwisko = reader.GetString(4);
+                    connection.Close();
+                } 
+                else 
+                {
+                    connection.Close();
+                    return NotFound();
+                }
+            }
+            return dto;
+        }
+    }
+}
